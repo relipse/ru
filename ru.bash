7db18fa0f8d803511cf6638a347a3006a3e972d4
James Kinsman <relipse@gmail.com>
2025-12-18 20:36:39 -0600

    Update ru.bash

--- ru.bash
+++ ru.bash
@@ -30,7 +30,7 @@ function ru() {
 # @author relipse
 # @license Dual License: Public Domain and The MIT License (MIT)
 #        (Use either one, whichever you prefer)
-# @version 2.85
+# @version 2.86
 ####################################################################
     # Reset all variables that might be set
     local verbose=0
